d29a07589136005528e96e1321c5b2a72502b569
Glaucyrlei Pereira da Silva <83191449+GanestGamer11@users.noreply.github.com>
2022-02-02 16:49:59 -0300

    2° Questão feita no dia 02/02/2022

---
+++ 1° Tutoria 1M/Questao2.java
@@ -0,0 +1,20 @@
+/*
+crie um progama que guarde os valores 75, 29,9 e 40 em 3 
+variaveis, depois, dentro de um mesmo println, imprima as 3 variaveis
+*/
+public class Questao2{
+	public static void main(String[] args) {
+		
+		int leprechau = 75;
+		double bacana = 29.9;
+		int glaucy = 40;
+
+		//String vela = "coisa";
+		//char nome = 'c';
+
+		System.out.println(leprechau+"\n"+ bacana+"\n" + glaucy);
+
+
+
+	}
+}
