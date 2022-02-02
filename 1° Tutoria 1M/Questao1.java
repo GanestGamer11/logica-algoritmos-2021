bad9e317d1b8280a45ac5f84017936eb7298a383
Glaucyrlei Pereira da Silva <83191449+GanestGamer11@users.noreply.github.com>
2022-02-02 16:49:30 -0300

    1° questão feita no dia 02/02/2022

---
+++ 1° Tutoria 1M/Questao1.java
@@ -0,0 +1,14 @@
+/*
+crie um progama que guarde os valores 75, 29,9 e 40 em 3 variaveis, 
+depois, dentro de um mesmo println, imprima as 3 variaveis
+*/
+public class Questao1{
+	public static void main(String[] args) {
+		
+		int macarronada = 10;
+		int miojo = 25;
+
+		System.out.println(macarronada);
+		System.out.println(miojo);
+	}
+}
